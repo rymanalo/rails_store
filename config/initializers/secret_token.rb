ae534a0063a28738d1d465c9a78f2557f83a7580
Ryan Manalo <ryan.manalo@alum.berkeley.edu>
2013-04-10 20:15:46 -0700

    Pass product_id to reviews.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsStore::Application.config.secret_token = '1f84d57bc64df11edcc95c435d88aac1db5cc9cc6ca31b911ef518580e453031991cc50cf0278670338c7284d184aeeae74f2e7982008495a0b2187cdbc65a9e'
